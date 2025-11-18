07448ca658bd7cee49c0634b4cc26acaf97953da
warisgulzar123 <warisaligulzar123@gmail.com>
2025-11-18 21:42:50 +0500

    backend-setupfull

--- backend/routers/auth_routers.js
+++ backend/routers/auth_routers.js
@@ -1,16 +1,11 @@
 import { Router } from "express";
 import { signupvalidation, loginvalidation } from "../middlewares/validation.js";
-import { signup } from '../controllers/authController.js';
+import { signup, login } from '../controllers/authController.js';
 
 
 const router = Router();
-
-router.post('/login',loginvalidation, (req, res) => {
-  res.send('Login successful');
-});
-
-router.post('/signup',signupvalidation, signup);
-
+router.post('/signup', signupvalidation, signup);
+router.post('/login', loginvalidation, login);
 
 
 export default router;
